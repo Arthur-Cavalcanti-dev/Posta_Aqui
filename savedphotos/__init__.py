aa4c9c30b75fcccb80f495fb4d3b8dcc3e439f5f
Arthur Cavalcanti <TechImperium.TI@gmail.com>
2025-03-18 23:05:39 -0300

    versão_0.6

--- savedphotos/__init__.py
+++ savedphotos/__init__.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 # Configuração do banco de dados
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
 app.config["SECRET_KEY"] = "034846e0b016caf2ef575a271b1e6ab0afed"
+app.config["UPLOAD_PHOTOS"] = "static/post"
 
 # Inicializando extensões
 db = SQLAlchemy(app)
